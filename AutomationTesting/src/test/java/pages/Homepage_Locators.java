ea054e605ef548508c3eaa4f3ebc2450ab8504d8
Veeru <Veeru@DESKTOP-KQ81DDT.Dlink>
2019-04-07 22:38:50 +0530

    TC_01: Checking homepage having three sliders are not

--- AutomationTesting/src/test/java/pages/Homepage_Locators.java
+++ AutomationTesting/src/test/java/pages/Homepage_Locators.java
@@ -1,5 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Homepage_Locators {
+	WebDriver driver;
+	By Shop=By.xpath("//a[contains(text(),'Shop')]");
+	
+	
+	
+	
+	public void shop() {
+		driver.findElement(Shop).click();
+	}
 
 }
